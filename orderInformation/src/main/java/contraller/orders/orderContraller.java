05801f6c1a76657342e3a69393a3f5d10c71058e
dilminaw <dilminadilshan9@gmail.com>
2025-05-26 12:07:50 +0530

    included crud operation

--- orderInformation/src/main/java/contraller/orders/orderContraller.java
+++ orderInformation/src/main/java/contraller/orders/orderContraller.java
@@ -5,6 +5,7 @@ import order.Order;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class orderContraller implements orderService{
@@ -18,30 +19,72 @@ public class orderContraller implements orderService{
             prepared.setObject(2,order.getCustomerName());
             prepared.setObject(3,order.getItemCode());
             prepared.setObject(4,order.getQuentity());
-            prepared.setObject(5,order.getPrice());
-            prepared.setObject(6,order.getDate());
+            prepared.setObject(5,order.getDate());
+            prepared.setObject(6,order.getPrice());
 
             prepared.executeUpdate();
+            return true;
 
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        return false;
+
     }
 
     @Override
     public boolean updateOrder(Order order) {
-        return false;
+        String sql="UPDATE orders set customer_name=?,item_code=?,quantity=?,order_date=?,total_price=? WHERE order_id=?";
+        try {
+            Connection connection = dbConnection.getInstance().getConnection();
+            PreparedStatement prepared = connection.prepareStatement(sql);
+            prepared.setObject(1,order.getCustomerName());
+            prepared.setObject(2,order.getItemCode());
+            prepared.setObject(3,order.getQuentity());
+            prepared.setObject(4,order.getDate());
+            prepared.setObject(5,order.getPrice());
+            prepared.setObject(6,order.getId());
+
+         return    prepared.executeUpdate()>0;
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public boolean deleteOrder(Integer id) {
-        return false;
+        try {
+            PreparedStatement prepared = dbConnection.getInstance().getConnection().prepareStatement("DELETE FROM orders WHERE order_id='" + id + "'");
+         return     prepared.executeUpdate()>0;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @Override
     public Order searchOrder(Integer id) {
+
+        try {
+            Connection connection = dbConnection.getInstance().getConnection();
+            PreparedStatement prepared = connection.prepareStatement("SELECT * FROM orders WHERE order_id='" + id + "'");
+            ResultSet resultSet = prepared.executeQuery();
+
+            if (resultSet.next()){
+                return new  Order(
+                        resultSet.getInt("order_id"),
+                        resultSet.getString("customer_name"),
+                        resultSet.getString("item_code"),
+                        resultSet.getInt("quantity"),
+                        resultSet.getDate("order_date").toLocalDate(),
+                        resultSet.getDouble("total_price")
+                );
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
         return null;
     }
 }
--- orderInformation/src/main/java/contraller/orders/ordersFormContraller.java
+++ orderInformation/src/main/java/contraller/orders/ordersFormContraller.java
@@ -5,19 +5,33 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import order.Order;
 
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 
-public class ordersFormContraller {
+
+public class ordersFormContraller implements Initializable {
 @FXML
     public TextField txtOrderDate;
+    public TableView<Order> tableorder2;
+    public TableColumn colId;
+    public TableColumn colcusname;
+    public TableColumn colItem;
+    public TableColumn colQuentity;
+    public TableColumn colDate;
+    public TableColumn colprice;
     @FXML
     private TextField txtCusID;
 
@@ -48,7 +62,7 @@ public class ordersFormContraller {
 
 
         if (orderService.addOrder(order)){
-
+            dataReload();
             new Alert(Alert.AlertType.CONFIRMATION,"ADDED COMPLETE");
         }
 
@@ -57,20 +71,98 @@ public class ordersFormContraller {
 
     @FXML
     void btnDeleteOnAction(ActionEvent event) {
+        if (orderService.deleteOrder(Integer.valueOf(txtID.getText()))){
+            new Alert(Alert.AlertType.CONFIRMATION,"DELETED");
+            dataReload();
+        }
 
     }
 
     @FXML
     void btnSearchOnAction(ActionEvent event) {
 
+        Order order= orderService.searchOrder(Integer.parseInt(txtID.getText()));
+        txtID.setText(""+order.getId());
+        txtCusID.setText(order.getCustomerName());
+        txtItem.setText(order.getItemCode());
+        txtQuentity.setText(""+order.getQuentity()) ;
+        txtOrderDate.setText(String.valueOf(order.getDate()) );
+        txtPrice.setText(""+order.getPrice());
+
     }
 
     @FXML
     void btnUpdateOnAction(ActionEvent event) {
+        Order order = new Order(Integer.parseInt(txtID.getText()) ,
+                txtCusID.getText(),txtItem.getText(),
+                Integer.parseInt(txtQuentity.getText()),
+                LocalDate.parse(txtOrderDate.getText()),
+                Double.parseDouble(txtQuentity.getText()));
 
+        if (orderService.updateOrder(order)){
+            new Alert(Alert.AlertType.CONFIRMATION,"updated success");
+            dataReload();
+        }
+
+    }
+    private void dataReload(){
+        ObservableList<Order> orderList = FXCollections.observableArrayList();
+
+        try {
+            Connection connection = dbConnection.getInstance().getConnection();
+            PreparedStatement prepared = connection.prepareStatement("SELECT * FROM orders");
+            ResultSet resultSet = prepared.executeQuery();
+
+            while (resultSet.next()){
+                Order order = new Order(resultSet.getInt("order_id"),
+                        resultSet.getString("customer_name"),
+                        resultSet.getString("item_code"),
+                        resultSet.getInt("quantity"),
+                        resultSet.getDate("order_date").toLocalDate(),
+                        resultSet.getDouble("total_price")
+                );
+                orderList.add(order);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        tableorder2.setItems(orderList);
     }
 
 
 
+    public void btnreload1(ActionEvent actionEvent) {
+        dataReload();
 
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        
+        colId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        colcusname.setCellValueFactory(new PropertyValueFactory<>("customerName"));
+        colItem.setCellValueFactory(new PropertyValueFactory<>("itemCode"));
+        colQuentity.setCellValueFactory(new PropertyValueFactory<>("quentity"));
+        colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+        colprice.setCellValueFactory(new PropertyValueFactory<>("price"));
+        
+        tableorder2.getSelectionModel().selectedItemProperty().addListener((observableValue, oldval, newval) -> {
+            
+            if (newval!=null){
+                inserTOtextfieldData(newval);
+            }
+            
+        });
+        dataReload();
+    }
+
+    private void inserTOtextfieldData(Order newval) {
+        txtID.setText((""+newval.getId()));
+        txtCusID.setText(newval.getCustomerName());
+        txtItem.setText(newval.getItemCode());
+        txtQuentity.setText(""+newval.getQuentity());
+        txtOrderDate.setText(String.valueOf(newval.getDate()));
+        txtPrice.setText(""+newval.getPrice());
+    }
 }
